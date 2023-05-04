1b7ec43c0e6f4713c146f0c207aed71a0ea5f430
Gustavo Murdiga <gumurdiga@gmail.com>
2023-05-04 19:50:11 -0300

    finished project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'api.tsx', 'api.ts'],
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,18 +1,32 @@
-import { globalStyles } from '@/styles/global'
-import type { AppProps } from 'next/app'
-import { Roboto } from 'next/font/google'
+import '../lib/dayjs'
 
-const roboto = Roboto({
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-})
+import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { globalStyles } from '../styles/global'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <main className={roboto.className}>
-      <Component {...pageProps} />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.ignite-call.rocketseat.com.br',
+            siteName: 'Ignite Call',
+          }}
+        />
+
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
